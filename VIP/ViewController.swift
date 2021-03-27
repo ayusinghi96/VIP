3a2660c5ab34f4fe57f750f381d840527fad30d2
Ayush Singhi <ayush.singhi@fiftyfivetech.io>
2021-03-27 11:23:02 +0530

    Initial Commit

---
+++ VIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIP
+//
+//  Created by 55 TECH on 27/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
